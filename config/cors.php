92cab8517230c09014413d5c7899b42cf4f94c84
Lucio-Lorenzetti <89593565+Lucio-Lorenzetti@users.noreply.github.com>
2026-07-23 20:24:57 -0300

    Correción archivo config/cors.php

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173')),
     'allowed_methods' => ['*'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
